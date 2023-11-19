87ab17e06b81495bdcd370c7f1c9b5ce57c708af
Suresh <sureshsol@gmail.com>
2023-11-20 07:37:24 +0800

    Changes to be committed: 	new file:   kubernetes-lab-setup/draft/get-hyperv-VM-mac.ps1 	deleted:    kubernetes-lab-setup/powershell/Kubernetes-Create-HyperV-VM-v1.1.ps1 	modified:   kubernetes-lab-setup/powershell/Kubernetes-Create-HyperV-VM.ps1 	deleted:    kubernetes-lab-setup/powershell/get-hyperv-VM-mac.ps1 	deleted:    kubernetes-lab-setup/powershell/vms - Copy.csv

---
+++ kubernetes-lab-setup/draft/get-hyperv-VM-mac.ps1
@@ -0,0 +1,95 @@
+﻿# PowerShell Script 
+# Script: get-hyperv-VM-mac.ps1
+# Description: Script to get Hyper-V VM MAC addresses to create user-data blank files.
+# Author: Suresh Solomon ( sureshsolomon@yahoo.com)
+# Date: November 15, 2023
+
+<#
+Disclaimer:
+This script is provided as-is without any warranty. Use it at your own risk.
+The author and contributors are not responsible for any damages or data loss caused by the script.
+Please review and understand the script before execution. Make sure to backup your data and test the script in a non-production environment.
+
+Usage:
+1. Set the execution policy to RemoteSigned before running the script.
+2. Modify the CSV file (vms.csv) with your VM configurations.
+3. Run the script to create Hyper-V VMs based on the provided configurations.
+
+#>
+
+# Get the directory containing the script
+$scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+
+# Set the path to the script file
+set-location  $scriptDirectory
+
+
+# Get all virtual machines
+$vms = Get-VM
+
+# Iterate through each virtual machine
+foreach ($vm in $vms) {
+    # Get MAC address of the first network adapter
+    $macAddress = $vm | Get-VMNetworkAdapter | Select-Object -First 1 | Select-Object -ExpandProperty MacAddress
+
+    # Get server name (Hyper-V host name)
+    $serverName = $env:COMPUTERNAME
+
+    # Output results
+    Write-Host "VM Name: $($vm.Name)"
+    Write-Host "MAC Address: $macAddress"
+    Write-Host "Server Name: $serverName"
+
+    # Check if MAC address is 00:00:00:00:00:00
+    if ($macAddress -eq '000000000000') {
+        Write-Host "Starting the VM to generate a valid MAC address..."
+        Start-VM -Name $vm.Name
+        # You might want to add a sleep here to allow time for the VM to generate a valid MAC address
+        # Start-Sleep -Seconds 60
+        Stop-VM -Name $vm.Name -Force
+        Write-Host "VM has been powered off after obtaining a valid MAC address."
+    } else {
+        Write-Host "MAC address is valid. No action needed."
+    }
+
+    Write-Host "----------------------"
+}
+
+Clear-Content .\mac-address-list.csv
+foreach ($vm in $vms) {
+    # Get MAC address of the first network adapter
+    $macAddress = $vm | Get-VMNetworkAdapter | Select-Object -First 1 | Select-Object -ExpandProperty MacAddress
+
+    # Remove "k8s-" prefix from the VM name
+    $vmName = $vm.Name -replace '^k8s-', ''
+
+    # Format DHCP configuration with MAC address in XX:XX:XX:XX:XX:XX format
+    $formattedMacAddress = ($macAddress -replace '(.{2})(.{2})(.{2})(.{2})(.{2})(.{2})', '$1:$2:$3:$4:$5:$6')
+    $formattedMacAddressfn = ($macAddress -replace '(.{2})(.{2})(.{2})(.{2})(.{2})(.{2})', '$1-$2-$3-$4-$5-$6')
+
+    # Convert MAC address to lowercase
+    $formattedMacAddressfnLower = $formattedMacAddressfn.ToLower()
+
+    $dhcpConfig = @"
+host $vmName {
+    hardware ethernet $formattedMacAddress;
+    #fixed-address 192.168.100.250;
+    option host-name "$vmName";
+}
+"@
+    $mac = " $vmName,$formattedMacAddressfnLower"
+
+
+    # Output DHCP configuration
+    #Write-Host $dhcpConfig
+    Write-Host $mac 
+    $mac | Out-File   .\mac-address-list.csv -Append
+    
+    $userdatafile=".\srv\autoinstall\node\user-data-$formattedMacAddressfnLower"
+    if (-not (Test-Path $userdatafile)) {
+    # File doesn't exist, create it
+    New-Item -ItemType File -Path $userdatafile
+    Write-Host "File created: $userdatafile"
+} 
+}
--- kubernetes-lab-setup/powershell/Kubernetes-Create-HyperV-VM.ps1
+++ kubernetes-lab-setup/powershell/Kubernetes-Create-HyperV-VM.ps1
@@ -1,24 +1,73 @@
-﻿# date January 2023
-# script to generate HyperV VM image for 1 x Management node and 2 x k3s single nodes
-# This script will create the VM with the specifications below
-# CPU 1
-# Memory 1GB
-# customize as required
-# Validated
-# 1. Drive letter ( chose the drive with most disk space)
-# 2. The iso filename to match downloaded iso name 
-# 3. I prefixed "demo-" to the hostnames  to record a video, this can be removed to shorten the name.
+﻿# PowerShell Script 
+# Script: Create-HyperVVM.ps1
+# Description: Script to create Hyper-V VMs based on configurations in a CSV file
+# Author: Suresh Solomon ( sureshsolomon@yahoo.com)
+# Date: November 15, 2023
 
-# ExecutionPolicy
-# if you have not executed a Powershell script before it may have been disabled
-# Familiarize yourselve with the impact of doing this.
-# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_execution_policies?view=powershell-7.3
+<#
+Disclaimer:
+This script is provided as-is without any warranty. Use it at your own risk.
+The author and contributors are not responsible for any damages or data loss caused by the script.
+Please review and understand the script before execution. Make sure to backup your data and test the script in a non-production environment.
 
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+Usage:
+1. Set the execution policy to RemoteSigned before running the script.
+2. Modify the CSV file (vms.csv) with your VM configurations.
+3. Run the script to create Hyper-V VMs based on the provided configurations.
+
+#>
 
 # create working folder
 # this script will default to  F:\kubernetes-project-lab
-$workingfolder = "F:\kubernetes-project-lab\"
+# To change this path search for 'F:\kubernetes-project-lab' replace with the path 
+# There is only one location to change under # create working folder
+# Change the Drive letter to on that exists on your computer,
+# You will also need to  update External Network name  for $switchInternet="internet"
+##  Hyper-V > Virtual Switch Manager > Identify the External Switch name
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+<#
+This script will read from a file called vms.csv
+Copy the content below and paste in a file vms.csv 
+Feel free to edite the VM name to your preference
+
+
+example content of "vms.csv"
+VMName,NetworkSwitch1,NetworkSwitch2,DiskSizeGB1,DiskSizeGB2,CPUCount,MemoryGB,ISOPath,BootOrder
+aaa-alpine1,Internet,"Private 192.168.100.0/24",50,,1,1,alpine-standard-3.18.4-x86_64.iso,DVD
+aaa-loadbalancer,,"Private 192.168.100.0/24",50,,1,1,alpine-standard-3.18.4-x86_64.iso,DVD
+aaa-master1,,"Private 192.168.100.0/24",50,,1,1,ubuntu-20.04.6-live-server-amd64.iso,Network
+aaa-master1,,"Private 192.168.100.0/24",50,,1,1,ubuntu-20.04.6-live-server-amd64.iso,Network
+aaa-master2,,"Private 192.168.100.0/24",50,,1,1,ubuntu-20.04.6-live-server-amd64.iso,Network
+aaa-master3,,"Private 192.168.100.0/24",50,,1,1,ubuntu-20.04.6-live-server-amd64.iso,Network
+aaa-worker1,,"Private 192.168.100.0/24",50,30,1,1,ubuntu-20.04.6-live-server-amd64.iso,Network
+aaa-worker2,,"Private 192.168.100.0/24",50,30,1,1,ubuntu-20.04.6-live-server-amd64.iso,Network
+aaa-worker3,,"Private 192.168.100.0/24",50,30,1,1,ubuntu-20.04.6-live-server-amd64.iso,Network
+aaa-xsinglenode,,"Private 192.168.100.0/24",50,30,1,1,ubuntu-20.04.6-live-server-amd64.iso,Network
+
+##
+NetworkSwitch1 : this is the internet connection . Please change internet to the name of your external Network on Hyper-V
+NetworkSwitch2 : this is the private network "Private 192.168.100.0/24"
+ISOPath : if there is a DVD loaded the iso name is listed here.
+BootOrder : 
+DVD > First Boot DVD, Second Boot HDD
+Network > First Boot Network  "Private 192.168.100.0/24" , Second Boot HDD
+#>
+
+# Get the directory containing the script
+$scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+
+# Set the path to the CSV file
+$csvPath = "$scriptDirectory\vms.csv"
+
+# Read the CSV file
+$vms = Import-Csv -Path $csvPath
+
+# create working folder
+#this is where the Hyper-V files will be created.
+# this script will default to  F:\kubernetes-project-lab
+$workingfolder = "F:\kubernetes-project-lab"
 if (!(Test-Path -Path "$workingfolder" -PathType Container)) {
         New-Item -Path "$workingfolder" -ItemType Directory
     }
@@ -30,7 +79,7 @@ if (!(Test-Path -Path "$isofolder" -PathType Container)) {
         New-Item -Path "$isofolder" -ItemType Directory
     }
 
-
+## Download alpine iso
 $outputPath = $DVDalpine
 $url = "https://dl-cdn.alpinelinux.org/alpine/v3.18/releases/x86_64/alpine-standard-3.18.4-x86_64.iso"
 $DVDalpine=Join-Path $isofolder (Split-Path $url -Leaf) # extracts filename from downloadurl
@@ -40,172 +89,107 @@ if (!(Test-Path -Path "$DVDalpine")) {
         Invoke-WebRequest -Uri $url -OutFile $outputPath
     }
 
-#Invoke-WebRequest -Uri $url -OutFile $outputPath
+
+## Download Ubunto iso
 $outputPath = $DVDUBUNTU
 $url = "https://releases.ubuntu.com/focal/ubuntu-20.04.6-live-server-amd64.iso"
-$DVDUBUNTU = Join-Path $isofolder (Split-Path $url -Leaf) # extracts filename from downloadurl #$DVDUBUNTU="$isofolder\faicd64-large_6.0.3.iso"
+$DVDUBUNTU = Join-Path $isofolder (Split-Path $url -Leaf) # extracts filename from download
 
 # download if file does not already exist
 if (!(Test-Path -Path "$DVDUBUNTU")) {
         Invoke-WebRequest -Uri $url -OutFile $outputPath
     }
 
+## change the name to the switch that has access to the internet.
+##  Hyper-V > Virtual Switch Manager > Identify the Switch names
 
-##
-# These are the Hyper-V virtual switch that must be created
-## all the VM's will have both internet and Private switch connected.
-## the internet switch will act as our management network so we are able to access the 
-
-
-## change this to the switch that has access to the internet.
 $switchInternet="internet" 
+
 
 ## this subnet is used for this example this is a label so any other subnet can be used for the VM's.
 $switchprivate="Private 192.168.100.0/24" 
-
 ##
-
 ### Creates new private network.
-
 $existingSwitch = Get-VMSwitch -Name $switchprivate -SwitchType Internal -ErrorAction SilentlyContinue
-if ($existingSwitch.name -eq $null) {
+if ($null -eq $existingSwitch.name) {
     New-VMSwitch -Name $switchprivate -SwitchType Internal -Notes "created for k8s-lab project "
 } else {
     Write-Host "The switch '$switchprivate' already exists."
 }
 
+# VM creation begins here.
 
-$hostnamelist=("k8s-alpine1,k8s-loadbalancer").Split(",")
-#get-vm | Remove-VM -Confirm $false
+foreach ($vm in $vms) {
+    $vmfolder = "$workingfolder\VirtualMachines\$($vm.VMName)"
+    # Intialize variable typess to enable provisioning of Hyper-V VM.   
+    [int64]$RAM=$null
+    [int64]$RAMmax=$null
+    [System.UInt64]$Disksize1=$null
+    [System.UInt64]$Disksize2=$null
 
-foreach ($vmname in $hostnamelist){
+    # Check if the VM already exists
+    $existingVM = Get-VM -Name  $vm.VMName -ErrorAction SilentlyContinue
 
-        $vmfolder = "$workingfolder\VirtualMachines\$vmname\"
-        # VM CPU and Memory config
-        New-VM -Name $vmname -MemoryStartupBytes 1GB -Generation 2 -Path "$vmfolder"
-        Set-VM -VMName $vmname -ProcessorCount 1 -DynamicMemory -MemoryMaximumBytes 2GB  -MemoryStartupBytes 1GB
+    if ($null -eq $existingVM) {
+        # VM does not exist, create a new one
+        New-VM -Name  $vm.VMName -Generation 2 -Path $vmFolder
+        $RAM = (1GB * $vm.MemoryGB)
+        $RAMmax = (2GB * $vm.MemoryGB)
+        Set-VM -Name  $vm.VMName -MemoryStartupBytes $RAM 
+        Set-VM -VMName  $vm.VMName -ProcessorCount $vm.CPUCount # -DynamicMemory -MemoryMaximumBytes $RAMmax -MemoryStartupBytes $RAM
+        Write-Host $vm.VMName "created successfully."
 
-        # Hard disk Primary OS 
-        New-VHD -Path "$vmfolder\$vmname-hdd1.vhdx" -SizeBytes 50GB -Dynamic
-        Add-VMHardDiskDrive -VMName $vmname -Path "$vmfolder\$vmname-hdd1.vhdx" -ControllerType SCSI   -ControllerLocation 0
-        # Load ISO drive for installation
-        add-VMDvdDrive -VMName $vmname  -Path "$DVDalpine" # validate iso filename to match downloaded name 
-        
+    # Hard disk Primary OS
+    [System.UInt64]$Disksize1=([long]$vm.DiskSizeGB1 * 1GB)
+    [System.UInt64]$Disksize2=([long]$vm.DiskSizeGB2 * 1GB)
+    $diskPath1 = Join-Path $vmfolder "$($vm.VMName)-hdd1.vhdx"
+    New-VHD -Path $diskPath1 -SizeBytes $Disksize1 -Dynamic 
+    Add-VMHardDiskDrive -VMName $vm.VMName -Path $diskPath1 -ControllerType SCSI -ControllerLocation 0
+
+    # Additional Hard disk
+    if ($vm.DiskSizeGB2 -ne "") {
+    $diskPath2 = Join-Path $vmfolder "$($vm.VMName)-hdd2.vhdx"
+    New-VHD -Path $diskPath2 -SizeBytes $Disksize2 -Dynamic
+    Add-VMHardDiskDrive -VMName $vm.VMName -Path $diskPath2 -ControllerType SCSI -ControllerLocation 1
+    } else {
+    "Disk2 not required"
+    }
+
+    # Load ISO drive for installation
+    $isoPath = Join-Path "$workingfolder\iso" $($vm.ISOPath)
+    add-VMDvdDrive -VMName $vm.VMName -Path $isoPath -ControllerLocation 3
+
+    #Network adapters
+    if($vm.NetworkSwitch1 -ne "") {
+    add-VMNetworkAdapter -VMName $vm.VMName -SwitchName $vm.NetworkSwitch1
+    add-VMNetworkAdapter -VMName $vm.VMName -SwitchName $vm.NetworkSwitch2
+    } else {
+    add-VMNetworkAdapter -VMName $vm.VMName -SwitchName $vm.NetworkSwitch2
+    }
+    #remove the network adapter without a SwitchName from the VM
+    get-VMNetworkAdapter  -VMName  $vm.VMName | Where-Object {$_.SwitchName -ne $switchprivate -and $_.SwitchName -ne $switchInternet  } |Remove-VMNetworkAdapter   
+
+    # Set boot order
+    $bootOrder = @()
+    if ($vm.BootOrder -eq "DVD") {
+        $bootOrder += Get-VMDvdDrive -VMName $vm.VMName
+        $bootOrder += Get-VMHardDiskDrive -VMName $vm.VMName | Where-Object { $_.ControllerLocation -eq 0 }
+    }
+    elseif ($vm.BootOrder -eq "Network") {
+        $bootOrder += Get-VMNetworkAdapter -VMName $vm.VMName| Where-Object {$_.SwitchName -eq $switchprivate}
+        $bootOrder += Get-VMHardDiskDrive -VMName $vm.VMName | Where-Object { $_.ControllerLocation -eq 0 }
+    }
 
 
-        if ($vmname -eq "k8s-alpine1") {
-            # Connect to HyperV Switch connected to External network.
-            connect-VMNetworkAdapter -VMName $vmname  -SwitchName $switchInternet
-            # add new NIC for Private Network
-            add-VMNetworkAdapter -VMName $vmname  -SwitchName $switchprivate
-            $vmNetworkAdapter = get-VMNetworkAdapter -VMName $vmname | where-object{$_.SwitchName -eq $switchprivate}
-            $vmHardDiskDrive = get-VMHardDiskDrive -VMName $vmname -ControllerType SCSI   -ControllerLocation 0
-        } else {
-            get-VMNetworkAdapter  -VMName $vmname | where {$_.SwitchName -ne $switchprivate } |Remove-VMNetworkAdapter        
-            add-VMNetworkAdapter -VMName $vmname  -SwitchName $switchprivate
-            $vmNetworkAdapter = get-VMNetworkAdapter -VMName $vmname | where-object{$_.SwitchName -eq $switchprivate}
-            $vmHardDiskDrive = get-VMHardDiskDrive -VMName $vmname -ControllerType SCSI   -ControllerLocation 0
-        }
-        # Turn off Secure Boot to simplify setup and set boot order  ( for generation 2 VM type)
-        #Set-VMFirmware $vmname -BootOrder  $(get-VMDvdDrive  $vmname), $(get-VMHardDiskDrive $vmname | where {$_.ControllerLocation -eq "0"})  -EnableSecureBoot Off
-        if ($vmname -ne "k8s") {
-            Set-VMFirmware $vmname -BootOrder  $(get-VMDvdDrive  $vmname), $(get-VMHardDiskDrive $vmname | Where-Object{$_.ControllerLocation -eq "0"})  -EnableSecureBoot Off
-        } else {
-            Set-VMFirmware "$vmname" -EnableSecureBoot Off -BootOrder $vmNetworkAdapter, $vmHardDiskDrive
-        }
+    # Set the boot order for the VM
+    Set-VMFirmware -VMName $vm.VMName -BootOrder $bootOrder -EnableSecureBoot Off
+    # Disable checkpoints 
+    Set-VM -Name $vm.VMName -CheckpointType Disabled
+
+    } else {
+        Write-Host $vm.VMName "already exists."
+    }
+
+
 
 }
-
-
-$hostnamelist=("k8s-master1,k8s-master2,k8s-master3").Split(",")
-
-foreach ($vmname in $hostnamelist){
-        $vmfolder = "$workingfolder\VirtualMachines\$vmname\"
-        $switch="ExternalNetwork"
-        # VM CPU and Memory config
-        New-VM -Name $vmname -MemoryStartupBytes 1GB -Generation 2 -Path "$vmfolder"
-        Set-VM -VMName $vmname -ProcessorCount 1 -DynamicMemory -MemoryMaximumBytes 2GB  -MemoryStartupBytes 1GB
-
-        # Hard disk Primary OS 
-        New-VHD -Path "$vmfolder\$vmname-hdd1.vhdx" -SizeBytes 50GB -Dynamic
-        Add-VMHardDiskDrive -VMName $vmname -Path "$vmfolder\$vmname-hdd1.vhdx" -ControllerType SCSI   -ControllerLocation 0
-
-
-        # Load ISO drive for installation
-        add-VMDvdDrive -VMName $vmname  -Path "$DVDUBUNTU" # validate iso filename to match downloaded name 
-
-        # Connect to HyperV Switch connected to External network.
-        #connect-VMNetworkAdapter -VMName $vmname  -SwitchName $switchInternet
-        #Remove External network NIC from VM
-        get-VMNetworkAdapter  -VMName $vmname | where {$_.SwitchName -ne $switchprivate } |Remove-VMNetworkAdapter        
-        # add new NIC for Private Network
-        add-VMNetworkAdapter -VMName $vmname  -SwitchName $switchprivate
-        $vmNetworkAdapter = get-VMNetworkAdapter -VMName $vmname | where-object{$_.SwitchName -eq $switchprivate}
-        $vmHardDiskDrive = get-VMHardDiskDrive -VMName $vmname -ControllerType SCSI   -ControllerLocation 0
-        # Turn off Secure Boot to simplify setup and set boot order ( for generation 2 VM type)
-        $vmDvdDrive = get-VMDvdDrive  $vmname
-        $vmHardDiskDrive = get-VMHardDiskDrive -VMName $vmname -ControllerType SCSI   -ControllerLocation 0
-        Set-VMFirmware $vmname -BootOrder  $vmDvdDrive, $vmHardDiskDrive -EnableSecureBoot Off
-
-}
-
-
-$hostnamelist=("k8s-worker1,k8s-worker2,k8s-worker3").Split(",")
-
-foreach ($vmname in $hostnamelist){
-        $vmfolder = "$workingfolder\VirtualMachines\"
-        $switch="ExternalNetwork"
-        # VM CPU and Memory config
-        New-VM -Name $vmname -MemoryStartupBytes 1GB -Generation 2 -Path "$vmfolder"
-        Set-VM -VMName $vmname -ProcessorCount 1 -DynamicMemory -MemoryMaximumBytes 2GB  -MemoryStartupBytes 1GB
-
-        # Hard disk Primary OS 
-        New-VHD -Path "$vmfolder\$vmname\$vmname-hdd1.vhdx" -SizeBytes 50GB -Dynamic
-        Add-VMHardDiskDrive -VMName $vmname -Path "$vmfolder\$vmname\$vmname-hdd1.vhdx" -ControllerType SCSI   -ControllerLocation 0
-        
-        # Hard disk secondary for k3s data 
-        New-VHD -Path "$vmfolder\$vmname\$vmname-hddadd.vhdx" -SizeBytes 60GB -Dynamic
-        Add-VMHardDiskDrive -VMName $vmname -Path "$vmfolder\$vmname\$vmname-hddadd.vhdx" -ControllerType SCSI  #-ControllerLocation 1
-        
-        #Remove External network NIC from VM
-        get-VMNetworkAdapter  -VMName $vmname | where {$_.SwitchName -ne $switchprivate } |Remove-VMNetworkAdapter
-        # add new NIC for Private Network
-        add-VMNetworkAdapter -VMName $vmname  -SwitchName $switchprivate
-        # Turn off Secure Boot to simplify setup and set boot order ( for generation 2 VM type)
-        
-        $vmNetworkAdapter = get-VMNetworkAdapter -VMName $vmname | where-object{$_.SwitchName -eq $switchprivate}
-        $vmHardDiskDrive = get-VMHardDiskDrive -VMName $vmname -ControllerType SCSI   -ControllerLocation 0
-
-        Set-VMFirmware "$vmname" -EnableSecureBoot Off -BootOrder $vmNetworkAdapter, $vmHardDiskDrive
-}
-
-$hostnamelist=("k8s-xsinglenode").Split(",")
-
-foreach ($vmname in $hostnamelist){
-        $vmfolder = "$workingfolder\VirtualMachines\"
-        $switch="ExternalNetwork"
-        # VM CPU and Memory config
-        New-VM -Name $vmname -MemoryStartupBytes 1GB -Generation 2 -Path "$vmfolder"
-        Set-VM -VMName $vmname -ProcessorCount 1 -DynamicMemory -MemoryMaximumBytes 2GB  -MemoryStartupBytes 1GB
-
-        # Hard disk Primary OS 
-        New-VHD -Path "$vmfolder\$vmname\$vmname-hdd1.vhdx" -SizeBytes 50GB -Dynamic
-        Add-VMHardDiskDrive -VMName $vmname -Path "$vmfolder\$vmname\$vmname-hdd1.vhdx" -ControllerType SCSI   -ControllerLocation 0
-        
-        # Hard disk secondary for k3s data 
-        New-VHD -Path "$vmfolder\$vmname\$vmname-hddadd.vhdx" -SizeBytes 60GB -Dynamic
-        Add-VMHardDiskDrive -VMName $vmname -Path "$vmfolder\$vmname\$vmname-hddadd.vhdx" -ControllerType SCSI  #-ControllerLocation 1
-        
-        #Remove External network NIC from VM
-        get-VMNetworkAdapter  -VMName $vmname | where {$_.SwitchName -ne $switchprivate } |Remove-VMNetworkAdapter
-        # add new NIC for Private Network
-        add-VMNetworkAdapter -VMName $vmname  -SwitchName $switchprivate
-        # Turn off Secure Boot to simplify setup and set boot order ( for generation 2 VM type)
-        
-        $vmNetworkAdapter = get-VMNetworkAdapter -VMName $vmname | where-object{$_.SwitchName -eq $switchprivate}
-        $vmHardDiskDrive = get-VMHardDiskDrive -VMName $vmname -ControllerType SCSI   -ControllerLocation 0
-
-        Set-VMFirmware "$vmname" -EnableSecureBoot Off -BootOrder $vmNetworkAdapter, $vmHardDiskDrive
-}
-
-
