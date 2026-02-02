b49c4d08ebd940e7edfd8649f78f6a9a0dfe79d3
root <root@alpine1.my.domain>
2026-02-02 17:41:36 +0800

    new scripts for single node VM creation

---
+++ srv/scripts/hyper-v_Host-singlenode/get-hyperv-start-stopVM.ps1
@@ -0,0 +1,37 @@
+﻿# Get the directory containing the script
+$scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+
+# Set the path to the script file
+set-location  $scriptDirectory
+
+
+# Get all virtual machines
+$vms = Get-VM
+
+# Iterate through each virtual machine
+#start VM
+foreach ($vm in $vms) {
+    # Check if MAC address is 00:00:00:00:00:00
+      if (($vm.State -ne 'Running') -and ($vm.Name -like "k3s*")) {
+        Write-Host $vm.Name +" Starting the VM "
+        Start-VM -Name $vm.Name
+        #Stop-VM -Name $vm.Name
+
+    } 
+
+    Write-Host "----------------------"
+}
+
+#stop VM
+foreach ($vm in $vms) {
+    # Check if MAC address is 00:00:00:00:00:00
+      if (($vm.State -eq 'Running') -and ($vm.Name -like "k3s*")) {
+        Write-Host $vm.Name +" stopping the VM "
+        #Start-VM -Name $vm.Name
+        Stop-VM -Name $vm.Name -TurnOff
+
+    } 
+
+    Write-Host "----------------------"
+}
